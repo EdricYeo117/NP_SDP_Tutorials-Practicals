de9d7485add0607b4b6537f5bca56b9d6c20ac36
EdricYeo117 <edricyeo.jr@gmail.com>
2024-11-25 15:43:51 +0800

    Changes to practical 7, refactoring player being passed into subclass instead of superclass (abstract)

---
+++ Test1/ConsoleApp1/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Customer : IObserver
+    {
+        public string customerName { get; private set; }
+
+
+        // Constructor
+        public Customer(string name)
+        {
+            customerName = name;
+        }
+
+        // Update method to receive notification from the subject
+        public void Update(string shopName, Product product, bool addOrRemove)
+        {
+            if (addOrRemove)
+            {
+                Console.WriteLine($"Hey {customerName}, {shopName} has added a new product: \n- {product.Name}!");
+            }
+            else
+            {
+                Console.WriteLine($"Hey {customerName}, {shopName} is no longer selling the product: \n- {product.Name}!");
+            }
+        }
+    }
+}
---
+++ Test1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+void main ()
+{
+    Shop shop1 = new Shop("Board Game Bits");
+    Shop shop2 = new Shop("Fnacy Furniture");
+
+    Customer customer1 = new Customer("John");
+    shop1.RegisterObserver(customer1);
+    shop2.RegisterObserver(customer1);
+
+    Customer customer2 = new Customer("Siti");
+    shop1.RegisterObserver(customer2);
+
+    Customer customer3 = new Customer("Ah Huat");
+    shop2.RegisterObserver(customer3);
+
+    Product product1 = new Product("Metal Houses for Monopoly");
+    shop1.addProduct(product1);
+
+    Product product2 = new Product("Razzer Awesome gaming chair");
+    shop2.addProduct(product2);
+
+    shop1.RemoveObserver(customer1);
+    shop1.removeProduct(product1);
+
+}
+
+
+main();
