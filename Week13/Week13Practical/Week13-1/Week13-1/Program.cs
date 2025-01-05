3d1245d15a3f8e521fca9eb6756e1a42f9194763
YJR117 <133361144+EdricYeo117@users.noreply.github.com>
2025-01-05 19:00:17 +0800

    Week 13 Practical

---
+++ Week13/Week13Practical/Week13-1/Week13-1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Week13_1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create some files
+        MediaFile soundFile = new MP3File("song.mp3");
+        MediaFile movieFile = new MP4File("movie.mp4");
+
+        // Create the original players
+        MP3Player mp3Player = new MP3Player();
+        MP4Player mp4Player = new MP4Player();
+
+        // Create adapters that make them conform to MediaPlayer
+        MediaPlayer mp3Adapter = new MP3PlayerAdapter(mp3Player);
+        MediaPlayer mp4Adapter = new MP4PlayerAdapter(mp4Player);
+
+        // Use the adapters via the MediaPlayer interface
+        MediaPlayer mp;
+
+        mp = mp3Adapter;
+        mp.Play(soundFile);
+        // Expected: "Playing MP3 File: song.mp3"
+
+        mp = mp4Adapter;
+        mp.Play(movieFile);
+        // Expected:
+        //   "Decompressing MP4 File: movie.mp4"
+        //   "Playing MP4 File: movie.mp4"
+    }
+}
