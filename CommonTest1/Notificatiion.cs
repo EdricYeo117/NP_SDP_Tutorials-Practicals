cd233a9500653bcf509d229da4fe24bdefb34a14
EdricYeo117 <edricyeo.jr@gmail.com>
2025-01-06 14:04:53 +0800

    Common test 1

---
+++ CommonTest1/Notificatiion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTest1
+{
+    public interface Notification
+    {
+        void Send(string message, string userName);
+    }
+}
---
+++ CommonTest1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using CommonTest1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        NotificationFactory smsFactory = new SMSFactory();
+        NotificationFactory emailFactory = new EmailFactory();
+        List<User> users = new List<User>();
+
+        User user1 = new User("John", smsFactory);
+        users.Add(user1);
+        User user2 = new User("Mary", emailFactory);
+        users.Add(user2);
+
+        foreach (User user in users)
+        {
+            user.SendMessage("Test message");
+        }
+
+        user1.Factory = emailFactory;
+
+        foreach (User user in users)
+        {
+            user.SendMessage("Test message 2");
+        }
+    }
+}
